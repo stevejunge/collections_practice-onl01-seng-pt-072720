d169e504dc5a7d5723a2bf5f05866d9f11224aef
Stephen Junge <steve.junge@gmail.com>
2020-08-07 02:15:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,9 +22,8 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.each do |string|
-    string.insert (2, "$")
+  array.each do |item|
+    item[2] = "$"
   end
-  kesha_maker["Rick", "Stephanie", "John", "Megan"]
 end
   
